3c38cd8575e06e683dc40ac2d3f3485be857f993
Jin Hong <jnhg1016@gmail.com>
2021-04-29 12:48:11 +1000

    const variables added to maintain constant values

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+// External packages
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+
+// Internal NPMs
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Inquirer prompts for user reponses
+const questions = [
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
